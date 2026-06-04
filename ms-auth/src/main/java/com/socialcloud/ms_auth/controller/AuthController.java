88f04e0294e3244e928db1fc4063f192699be733
Victor <vi.salasa@duocuc.cl>
2026-06-04 02:40:32 -0400

    Agrego codigos en controller, properties y aplication.

--- ms-auth/src/main/java/com/socialcloud/ms_auth/controller/AuthController.java
+++ ms-auth/src/main/java/com/socialcloud/ms_auth/controller/AuthController.java
@@ -1,4 +1,27 @@
 package com.socialcloud.ms_auth.controller;
 
+import com.socialcloud.ms_auth.dto.AuthResponse;
+import com.socialcloud.ms_auth.dto.LoginRequest;
+import com.socialcloud.ms_auth.dto.RegisterRequest;
+import com.socialcloud.ms_auth.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthService service;
+
+    @PostMapping("/register")
+    public AuthResponse register(@Valid @RequestBody RegisterRequest request) {
+        return service.register(request);
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(@Valid @RequestBody LoginRequest request) {
+        return service.login(request);
+    }
 }
